9ef72ae1b987cdc8336c5a6b47bfae28231184a3
DispicableLee <lee.robert053@gmail.com>
2022-11-01 23:01:44 -0400

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+require "pry"
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+    def match(array)
+        aol = @word.split("")
+        p aol
+        helper = []
+        array.each do |s|
+            if s.split("").sort == aol.sort
+                helper.push(s)
+            end
+        end
+        p helper
+    end
+end
+
+listen = Anagram.new("listen")
+
+listen.match(["inlets", "blah", "enlists"])
+
